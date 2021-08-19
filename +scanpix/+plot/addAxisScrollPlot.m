5107da960fbd1df554c7d048ca3112b3e3d8999f
LaurenzMuessig <l.muessig@gmx.net>
2021-08-19 22:27:48 +0100

    updates
    improved plotting of waveform distribution on shank for npix data

--- +scanpix/+plot/addAxisScrollPlot.m
+++ +scanpix/+plot/addAxisScrollPlot.m
@@ -42,6 +42,7 @@ if isempty(ax)
     else
         hAx     = polaraxes('Parent',hScroll.hPan,'Units','pixels','Position',[plotPos(1) plotPos(2) plotPos(3:4)]);
     end
+    firstAxFlag = true;
 else
     
     % create the new axis
@@ -50,25 +51,36 @@ else
     else
         hAx     = polaraxes('Parent',hScroll.hPan,'Units','pixels','Position',[plotPos(1) plotPos(2) plotPos(3:4)]);
     end
-    %increase panel height to fit all figures if necessary
-    %current position of axis and canvas
-    posAx       = get(hAx,'Position'); % current axis size/position
-    p_hPan      = get(hScroll.hPan, 'Position'); % canvas size/position
-    
-    % horizontal size + slider
-    if posAx(1) + posAx(3) > 0.95*p_hPan(3)
-        set(hScroll.hPan, 'Position',[p_hPan(1:2) p_hPan(3)+plotPos(3)+plotSep(1) p_hPan(4)]); % increase canvas size
-        p_hPan      = get(hScroll.hPan, 'Position'); %
-        set(hScroll.hSldX, 'Max',p_hPan(3),'Enable','on'); % increase slider range
-    end
-    
-    % vertical size + slider
-    if posAx(2) + posAx(4) > 0.95*p_hPan(4)
-        set(hScroll.hPan, 'Position',[p_hPan(1:2) p_hPan(3) p_hPan(4)+plotPos(4)+plotSep(2)]);
-        p_hPan      = get(hScroll.hPan, 'Position'); %
-        set(hScroll.hSldY,'Max',p_hPan(4),'Value',0,'Enable','on'); % increase range
-    end
+    firstAxFlag = false;
 end
+
+%increase panel height to fit all figures if necessary
+%current position of axis and canvas
+posAx       = get(hAx,'Position'); % current axis size/position
+p_hPan      = get(hScroll.hPan, 'Position'); % canvas size/position
+
+% horizontal size + slider
+if posAx(1) + posAx(3) > 0.95*p_hPan(3)
+    if firstAxFlag
+        set(hScroll.hPan, 'Position',[p_hPan(1:2) 1.05*+plotPos(3)+plotSep(1) p_hPan(4)]); % increase canvas size
+    else
+        set(hScroll.hPan, 'Position',[p_hPan(1:2) p_hPan(3)+plotPos(3)+plotSep(1) p_hPan(4)]); % increase canvas size
+    end
+    p_hPan      = get(hScroll.hPan, 'Position'); %
+    set(hScroll.hSldX, 'Max',p_hPan(3),'Enable','on'); % increase slider range
+end
+
+% vertical size + slider
+if posAx(2) + posAx(4) > 0.95*p_hPan(4)
+    if firstAxFlag
+        set(hScroll.hPan, 'Position',[p_hPan(1:2) p_hPan(3) 1.05*plotPos(4)+plotSep(2)]);
+    else
+    	set(hScroll.hPan, 'Position',[p_hPan(1:2) p_hPan(3) p_hPan(4)+plotPos(4)+plotSep(2)]);
+    end
+    p_hPan      = get(hScroll.hPan, 'Position'); %
+    set(hScroll.hSldY,'Max',p_hPan(4),'Value',0,'Enable','on'); % increase range
+end
+
 
 % drawnow
 
--- +scanpix/+plot/plotWaveDistByLocOnShank.m
+++ +scanpix/+plot/plotWaveDistByLocOnShank.m
@@ -15,13 +15,26 @@ function plotWaveDistByLocOnShank(meanWaveforms,coords,chanMap)
 %
 % LM 2021
 
-% 
-scalingFact = (max(coords(:,2)) - min(coords(:,2)) ) / 6; % dividing by 6 seems to yield good size for waveforms, irrespective of length of probe that is plotted
+%%TO-DO:
+% test on different resolution machines
 
-figSz  = [200 850];
+% 
+channelSpacing = 20;
+scalingFact = (max(coords(:,2)) - min(coords(:,2)) ) / 15; % dividing by 15 seems to yield okay size for waveforms, irrespective of length of probe that is plotted 
+figSz  = [400 850]; % hard coded
+
+% scale axis if more than 600um of shank are plotted
+shankDist2PLot = (max(coords(:,2)) - min(coords(:,2)));
+if shankDist2PLot < 600
+    yScaling = 1;
+else
+    yScaling = shankDist2PLot / 600; % note hard coded!
+end
+% open scrollable figure
 screenSz = get(0,'ScreenSize');
-hFig = figure('Name','scaNpix::WaveformDistOnShank','Units','pixels','Position',[0.1*screenSz(3) 0.1*screenSz(4) figSz]);
-hAx = axes(hFig,'position',[0.25 0.05 0.7 0.9]);
+hScroll              = scanpix.plot.createScrollPlot([0.1*screenSz(3) 0.1*screenSz(4) figSz]); 
+hScroll.hFig.Name    = 'scaNpix::WaveformDistOnShank';
+hAx                  = scanpix.plot.addAxisScrollPlot( hScroll,[0.15*hScroll.hFig.Position(3) 0.05*hScroll.hFig.Position(4) 0.7*hScroll.hFig.Position(3) round((figSz(2)-50) * yScaling)], [0.15*hScroll.hFig.Position(3) 0.05*hScroll.hFig.Position(4)]);
 
 % need overall of max of waveform to scale data properly
 [maxVals, maxInd] = cellfun(@(x) max(abs(min(x,[],1) - max(x,[],1))),meanWaveforms,'uni',0);
@@ -49,11 +62,13 @@ colInd = randperm(size(col,1)); % randomise so colours don't cluster in groups
 hold(hAx,'on');
 for j = 1:length(meanWaveforms)
    
-    xStart = xBase(2,coords(j,1)==xBase(1,:));
-    plot(hAx,(xStart:xStart+nSamplesWaveform-1),-((meanWaveforms{j}(:,maxInd{j})./maxVals)*scalingFact)+coords(j,2),'color',col(colInd(j),:),'LineWidth',2);
+    xStart = xBase(2,coords(j,1)==xBase(1,:)) + randi(round([-nSamplesWaveform/4 nSamplesWaveform/4]),1); % jitter by a quarter of n waveform samles in x 
+    plot(hAx,(xStart:xStart+nSamplesWaveform-1),-((meanWaveforms{j}(:,maxInd{j})./maxVals)*scalingFact)+coords(j,2) + randi(round([-channelSpacing/2 channelSpacing/20]),1),'color',col(colInd(j),:),'LineWidth',2); % jitter by half of channel distance in y
 
 end
 hold(hAx,'off');
 
-set(hAx,'xlim',[-3 xBase(2,4)+nSamplesWaveform+3],'xtick',sort(xBase(2,:))+nSamplesWaveform/2,'xticklabel',sort(xBase(1,:)),'ylim',[min(coords(:,2))-scalingFact/4 max(coords(:,2))+scalingFact/2],'ytick',[min(coords(:,2)) max(coords(:,2))],'yticklabel',round([min(coords(:,2)),max(coords(:,2))]));  
+% set(hAx,'xlim',[-3 xBase(2,4)+nSamplesWaveform+3],'xtick',sort(xBase(2,:))+nSamplesWaveform/2,'xticklabel',sort(xBase(1,:)),'ylim',[min(coords(:,2))-scalingFact/4 max(coords(:,2))+scalingFact/2],'ytick',[]);%[min(coords(:,2)) max(coords(:,2))],'yticklabel',round([min(coords(:,2)),max(coords(:,2))]));  
+set(hAx,'xlim',[-3 xBase(2,4)+nSamplesWaveform+3],'xtick',[],'ylim',[min(coords(:,2))-scalingFact/4 max(coords(:,2))+scalingFact/2],'ytick',[]);%[min(coords(:,2)) max(coords(:,2))],'yticklabel',round([min(coords(:,2)),max(coords(:,2))]));  
+
 end
