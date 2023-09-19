e2234fa6a588d3a23a52b4eff9e478e2ed7a82c9
LaurenzMuessig <l.muessig@gmx.net>
2023-09-19 16:21:49 +0100

    Update spatialCorrelation.m

--- +scanpix/+analysis/spatialCorrelation.m
+++ +scanpix/+analysis/spatialCorrelation.m
@@ -1,4 +1,4 @@
-function r = spatialCorrelation(maps)
+function r = spatialCorrelation(mapsA,mapsB)
 % spatialCorrelation - Fully vectorised version of computing correlations between rate maps. 
 % package: scanpix.analysis
 %
@@ -19,72 +19,86 @@ function r = spatialCorrelation(maps)
 %    r = scanpix.analysis.spatialCorrelation(maps)
 %
 %  Inputs: 
-%    maps - cell array of rate maps - either {maps,1} or {mapsA,mapsB} 
+%    mapsA - cell array of rate maps - either {maps,1} or {mapsA,mapsB} 
 %           (make sure all maps are of same size)
 %
 %  Output: 
 %    r    - 1:nCells x 1:nCells array of correlations or 1:nCells array of
-%           correlations of trial A v B (see above for input format)
+%           pairwise correlations of maps A v B (see above for input format)
 %
 % LM 2020
 
 
 %% some input checks
-if ~iscell(maps)
-   error('Gimme some cell array of maps as input please, will ya?'); 
+if ~iscell(mapsA) || (nargin == 2 && ~iscell(mapsB))
+   error('Gimme some cell array(s) of maps as input please, will ya?'); 
 end
 
-if isempty(maps)
+if isempty(mapsA) || (nargin == 2  && isempty(mapsB))
    error('How would I get the correlations for an empty array of maps? Doesn''t work or does it?'); 
 end
 
-
-% switch between modes
-if size(maps,2) > 1
-    % A v B
-    maps   = vertcat(maps(:));
-    ABflag = true;
-    fprintf('Running ''scanpix.analysis.spatialCorrelation'' in A v B mode.\n');
-else
-    % the full monty
-    ABflag = false;
-end
-
-% interpolate maps to common size in case dimensions differ - this is
-% inspired from CBarry's code
-if size( unique( cell2mat( cellfun(@(x) size(x), maps, 'uni', 0) ), 'rows' ), 1) > 1 %
-    maxDimSz = max(unique( cell2mat( cellfun(@(x) size(x), maps, 'uni', 0) ), 'rows'));
-    for i=1:length(maps)
-        maps{i}=interp2(maps{i},linspace(1,size(maps{i},2), maxDimSz(2)),linspace(1, size(maps{i},1), maxDimSz(1))', '*linear');
+if nargin == 2
+   
+    sizeA = length(mapsA);
+    sizeB = length(mapsA);
+    if ~any(sizeA==1 | sizeB==1) || sizeA ~= sizeB
+        error('Dimensions of map arrays are wrong. You can either have a 1x1 map array vs. nx1 map array or an nx1 map array vs. nx1 map array! Now go back and try harder.')
+    end
+    
+    % interpolate maps to common size in case dimensions differ - this is
+    % inspired from CBarry's code
+    if size(unique([size(mapsA{1});size(mapsB{1})],'rows'),1) > 1
+        maxDimSz = max([size(mapsA{1});size(mapsB{1})]);
+        for i=1:length(mapsA)
+            mapsA{i}=interp2(mapsA{i},linspace(1,size(mapsA{i},2), maxDimSz(2)),linspace(1, size(mapsA{i},1), maxDimSz(1))', '*linear');
+        end
+        %
+        for i=1:length(mapsB)
+            mapsB{i}=interp2(mapsB{i},linspace(1,size(mapsB{i},2), maxDimSz(2)),linspace(1, size(mapsB{i},1), maxDimSz(1))', '*linear');
+        end
     end
 end
 
 %% get the correlations
-% this only does anything if you want to get A v B
-unVisInd = isnan(maps{1}) | isnan(maps{round(length(maps)/2)+1});
-unVisInd = repmat(unVisInd(:), 1, length(maps)); % get into correct format;
 
-% first arrange ratemaps in a ratemap_colVector x nCells array (so each column is one rate map)
-rMapArray           = reshape(horzcat(maps{:}), numel(maps{1}), length(maps));
-rMapArray(unVisInd) = NaN; % set unvisited to NaN (only relevant if you compare different trials)
-rMapArray           = bsxfun(@minus, rMapArray, nanmean(rMapArray, 1) ); % subtract mean/cell
+if nargin == 2
+    % Here we run A v B pairwise correlations
+    unVisInd = isnan(mapsA{1}) | isnan(mapsB{1});
+    unVisIndA = repmat(unVisInd, 1,1, length(mapsA)); % get into correct format;
+    unVisIndB = repmat(unVisInd, 1,1, length(mapsB)); % get into correct format;
 
-A         = rMapArray(:);                                                    % this is column vector of all rate maps ([rateMap_cell1; rateMap_cell2;...;rateMap_cellN])
-B         = repmat( rMapArray, length(maps), 1 );                            % these are all ratemap column vectors copied nCell times (row wise), so in the end is ratemap_colVector*nCells x nCells
-AB        = reshape( bsxfun(@times, A, B), numel(maps{1}), length(maps)^2 ); % need to reshape so each column corresponds to a ratemap col vector for doing the sums
-sumAB     = nansum(AB, 1);                                                   % This is a 1 x nCells^2 vector, one sum(AB) for each ratemap comparison.
-sqrAA     = sqrt( nansum(rMapArray.^2, 1) )';                                % only need to calculate once as product sqrt(AA) x sqrt(BB) == sqrt(AA) x transpose(sqrt(AA))
-sqrAABB   = sqrAA * sqrAA';
-r         = sumAB ./ sqrAABB(:)';                                            % this is a length(maps)^2 row vector of all correlations
-r         = reshape(r, length(maps), length(maps));                          % reshape into confusion matrix, so rows = 1:cellN vs column 1:cellN ( r(i,j) == r(j,i) )
-
-
-% A v B between cells (diagonal of upper right quadrant)  
-if ABflag
-    sz    = length(r)/2;
-    r     = diag(r(1:sz,sz+1:end));
+    rMapArrayA            = cat(3,mapsA{:});
+    rMapArrayA(unVisIndA) = NaN;
+    A                     = bsxfun(@minus, rMapArrayA , nanmean( nanmean(rMapArrayA ,1), 2 ) );
+    %
+    rMapArrayB            = cat(3,mapsB{:});
+    rMapArrayB(unVisIndB) = NaN;
+    B                     = bsxfun(@minus, rMapArrayB , nanmean( nanmean(rMapArrayB ,1), 2 ) );
+    %
+    AB = bsxfun(@times, A, B);
+    sumAB =  nansum(nansum(AB,1),2);                    % This is a 1x1xnMod vector, one sum(AB) for each model.
+    sqrAA = sqrt( nansum(nansum(rMapArrayA.^2,1),2) );           % This is a 1x1 integer
+    sqrBB = sqrt( nansum(nansum(rMapArrayB.^2,1),2) );           % This is a 1x1xnMod vector, one sqrt(B.^2) for each model.
+    r     = squeeze(sumAB ./ bsxfun(@times, sqrAA, sqrBB));
+    
+else
+    % Here we run all possible correlations in the map array
+    fprintf('Running all possible correlation in map array.\n');
+    
+    rMapArrayA = reshape(horzcat(mapsA{:}), numel(mapsA{1}), length(mapsA));
+    rMapArrayA = bsxfun(@minus, rMapArrayA, nanmean(rMapArrayA, 1) ); % subtract mean/cell
+    %    
+    A         = rMapArrayA(:);                                                     % this is column vector of all rate maps ([rateMap_cell1; rateMap_cell2;...;rateMap_cellN])
+    B         = repmat( rMapArrayA, length(mapsA), 1 );                            % these are all ratemap column vectors copied nCell times (row wise), so in the end is ratemap_colVector*nCells x nCells
+    AB        = reshape( bsxfun(@times, A, B), numel(mapsA{1}), length(mapsA)^2 ); % need to reshape so each column corresponds to a ratemap col vector for doing the sums
+    sumAB     = nansum(AB, 1);                                                     % This is a 1 x nCells^2 vector, one sum(AB) for each ratemap comparison.
+    sqrAA     = sqrt( nansum(rMapArrayA.^2, 1) )';                                 % only need to calculate once as product sqrt(AA) x sqrt(BB) == sqrt(AA) x transpose(sqrt(AA))
+    sqrAABB   = sqrAA * sqrAA';
+    r         = sumAB ./ sqrAABB(:)';                                            % this is a length(maps)^2 row vector of all correlations
+    r         = reshape(r, length(mapsA), length(mapsA));                          % reshape into confusion matrix, so rows = 1:cellN vs column 1:cellN ( r(i,j) == r(j,i) )
+ 
 end
-
 
 end
 
