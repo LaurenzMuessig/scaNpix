142c86bc6bddd5caadbb7d4ee1a3af6e132d7af0
LaurenzMuessig <l.muessig@gmx.net>
2024-07-10 16:35:21 +0100

    add sparsity calc

---
+++ +scanpix/+analysis/getSparsity.m
@@ -0,0 +1,27 @@
+function sparsity = getSparsity(rMap,posMap)
+% getSparsity - compute sparsity of rate map (Skaggs et al., 1996)
+% package: scanpix.analysis
+%
+%
+% Syntax:
+%       sparsity = scanpix.analysis.getSparsity(rMap,posMap)
+%
+% Inputs:
+%    rMap     - rate map
+%    posMap   - position map
+%
+% Outputs: 
+%
+%    sparsity - sparsity of rate map
+%
+% LM 2024
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+P_x = posMap ./ sum(posMap(:),'omitnan');
+%
+num   = sum(rMap(:) .* P_x(:), 'omitnan')^2;
+denom = sum(rMap(:).^2 .* P_x(:), 'omitnan');
+%
+sparsity = num / denom;
+
+end
